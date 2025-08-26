80ddfd2d10d53867c0533088a2800862e9009acd
SelinBeulah <selinbeulah01@gmail.com>
2025-08-26 12:11:20 +0530

    Problem - 2

---
+++ Problem2.js
@@ -0,0 +1,15 @@
+// Problem-2: With a single integer as the input, generate the following until a = x [series of numbers as shown in below examples]
+ 
+function odd_Func(a) {
+    let oddNum = [];
+    for(let i = 0; i < a; i++){
+        oddNum.push(2 * i +1)
+    }
+    console.log(oddNum.join(", "));
+    
+}
+
+odd_Func(1);
+odd_Func(2);
+odd_Func(3);
+odd_Func(4);
